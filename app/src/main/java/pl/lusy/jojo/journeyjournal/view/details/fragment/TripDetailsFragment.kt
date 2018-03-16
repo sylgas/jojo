4682dea49d9eedb211f802ee9c73fd03ff76bccc
sylgas <sylgas@poczta.fm>
2018-03-16 22:55:04 +0100

    Add date picker

--- app/src/main/java/pl/lusy/jojo/journeyjournal/view/details/fragment/TripDetailsFragment.kt
+++ app/src/main/java/pl/lusy/jojo/journeyjournal/view/details/fragment/TripDetailsFragment.kt
@@ -1,20 +1,84 @@
 package pl.lusy.jojo.journeyjournal.view.details.fragment
 
+import android.app.DatePickerDialog
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.fragment_trip_details.*
 import pl.lusy.jojo.journeyjournal.databinding.FragmentTripDetailsBinding
+import pl.lusy.jojo.journeyjournal.view.details.model.TripDetailsModel
+import java.text.DateFormat
+import java.util.*
+
+var TextView.date: Date
+    get() = DateFormat.getDateInstance().parse(text.toString())
+    set(value) {
+        text = DateFormat.getDateInstance().format(value)
+    }
 
 class TripDetailsFragment : DaggerFragment() {
     companion object {
         fun newInstance(): TripDetailsFragment = TripDetailsFragment()
     }
 
+    private val tripModel: TripDetailsModel by lazy {
+        ViewModelProviders.of(activity!!).get(TripDetailsModel::class.java)
+    }
+
+    private val startDateChangeListener =
+        DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            tripModel.onTripStartDateSet(year, month, dayOfMonth)
+        }
+
+    private val endDateChangeListener =
+        DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            tripModel.onTripEndDateSet(year, month, dayOfMonth)
+        }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return FragmentTripDetailsBinding.inflate(inflater, container!!, false).root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupView()
+        setupViewListeners()
+    }
+
+    private fun setupView() {
+        tripEndDate.setOnClickListener({
+            showSelectDateDialog(tripEndDate.date, endDateChangeListener)
+        })
+        tripStartDate.setOnClickListener({
+            showSelectDateDialog(tripStartDate.date, startDateChangeListener)
+        })
+    }
+
+    private fun showSelectDateDialog(date: Date, listener: DatePickerDialog.OnDateSetListener) {
+        val calendar = Calendar.getInstance()
+        calendar.time = date
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(context, listener, year, month, dayOfMonth).show()
+    }
+
+    private fun setupViewListeners() {
+        tripModel.tripStartDate.observe(this, Observer<Date?> { updateTripStartDate(it!!) })
+        tripModel.tripEndDate.observe(this, Observer<Date?> { updateTripEndDate(it!!) })
+    }
+
+    private fun updateTripEndDate(date: Date) {
+        tripEndDate.date = date
+    }
+
+    private fun updateTripStartDate(date: Date) {
+        tripStartDate.date = date
+    }
 }
--- app/src/main/java/pl/lusy/jojo/journeyjournal/view/details/model/TripDetailsModel.kt
+++ app/src/main/java/pl/lusy/jojo/journeyjournal/view/details/model/TripDetailsModel.kt
@@ -8,12 +8,20 @@ import io.reactivex.rxkotlin.addTo
 import io.reactivex.rxkotlin.subscribeBy
 import pl.lusy.jojo.journeyjournal.data.prefs.TripData
 import pl.lusy.jojo.journeyjournal.data.prefs.asSingle
+import java.util.*
 
 class TripDetailsModel : ViewModel() {
+
     private val compositeDisposable: CompositeDisposable = CompositeDisposable()
 
     private val mutableTripName: MutableLiveData<String> = MutableLiveData()
     val tripName: LiveData<String> = mutableTripName
+
+    private val mutableTripStartDate: MutableLiveData<Date> = MutableLiveData()
+    val tripStartDate: LiveData<Date> = mutableTripStartDate
+
+    private val mutableTripEndDate: MutableLiveData<Date> = MutableLiveData()
+    val tripEndDate: LiveData<Date> = mutableTripEndDate
 
     init {
         TripData.asSingle(TripData::name)
@@ -21,7 +29,21 @@ class TripDetailsModel : ViewModel() {
                 mutableTripName.value = it
             })
             .addTo(compositeDisposable)
+
+        TripData.asSingle(TripData::startDate)
+            .subscribeBy(onSuccess = {
+                mutableTripStartDate.value = getDateFromMillis(it)
+            })
+            .addTo(compositeDisposable)
+
+        TripData.asSingle(TripData::endDate)
+            .subscribeBy(onSuccess = {
+                mutableTripEndDate.value = getDateFromMillis(it)
+            })
+            .addTo(compositeDisposable)
     }
+
+    private fun getDateFromMillis(milis: Long) = if (milis == 0L) Date() else Date(milis)
 
     fun onTripNameChange(name: String) {
         mutableTripName.value = name
@@ -29,10 +51,26 @@ class TripDetailsModel : ViewModel() {
 
     fun onSave() {
         TripData.name = tripName.value ?: ""
+        TripData.startDate = tripStartDate.value?.time ?: 0L
+        TripData.endDate = tripEndDate.value?.time ?: 0L
     }
 
     override fun onCleared() {
         super.onCleared()
         compositeDisposable.clear()
+    }
+
+    fun onTripStartDateSet(year: Int, month: Int, dayOfMonth: Int) {
+        mutableTripStartDate.value = getDate(year, month, dayOfMonth)
+    }
+
+    fun onTripEndDateSet(year: Int, month: Int, dayOfMonth: Int) {
+        mutableTripEndDate.value = getDate(year, month, dayOfMonth)
+    }
+
+    fun getDate(year: Int, month: Int, dayOfMonth: Int): Date {
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month, dayOfMonth)
+        return calendar.time
     }
 }
